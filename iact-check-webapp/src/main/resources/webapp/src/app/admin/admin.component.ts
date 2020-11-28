68607504dcc861a051561da8385a4a228c0b44db
Yannick Huggler <yannick@huggler.io>
2020-11-28 13:46:08 +0100

    Setup proper routing for the nested modules

---
+++ iact-check-webapp/src/main/resources/webapp/src/app/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss'],
+})
+export class AdminComponent implements OnInit {
+  constructor(private keycloakService: KeycloakService) {}
+
+  ngOnInit(): void {
+    this.keycloakService.isLoggedIn().then((isLoggedIn) => {
+      if (!isLoggedIn) {
+        this.keycloakService.login().then();
+      }
+    });
+  }
+}
--- iact-check-webapp/src/main/resources/webapp/src/app/admin/admin.module.ts
+++ iact-check-webapp/src/main/resources/webapp/src/app/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin.routing.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AdminComponent } from './admin.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent, HeaderComponent, AdminComponent],
   imports: [CommonModule, AdminRoutingModule],
+  bootstrap: [AdminComponent]
 })
 export class AdminModule {}
