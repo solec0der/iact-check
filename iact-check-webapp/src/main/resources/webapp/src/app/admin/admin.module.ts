923fcc48b1f10ac739b5d807f57abe47cd3fb479
Yannick Huggler <yannick@huggler.io>
2020-11-28 09:25:18 +0100

    added prettierrc and formatted all files

--- iact-check-webapp/src/main/resources/webapp/src/app/admin/admin.module.ts
+++ iact-check-webapp/src/main/resources/webapp/src/app/admin/admin.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin.routing.module';
 
--- iact-check-webapp/src/main/resources/webapp/src/app/app-routing.module.ts
+++ iact-check-webapp/src/main/resources/webapp/src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
-      useHash: true,
     }),
   ],
   exports: [RouterModule],
