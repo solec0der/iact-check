843391b99621c9056731b47963663afc71849e89
Yannick Huggler <yannick@huggler.io>
2021-04-22 08:23:07 +0200

    cleaned up readme and fixed vulnerabilities

--- README.md
+++ README.md
@@ -2,10 +2,6 @@ Iact Check
 ==============
 
 ![Build and Analyze Core](https://github.com/solec0der/iact-check/workflows/Build%20and%20Analyze%20Core/badge.svg)
-[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=solec0der_iact-check&metric=alert_status&token=1d369feff79742df91970eb392253b10ed895e27)](https://sonarcloud.io/dashboard?id=solec0der_iact-check)
-[![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=solec0der_iact-check&metric=code_smells&token=1d369feff79742df91970eb392253b10ed895e27)](https://sonarcloud.io/dashboard?id=solec0der_iact-check)
-[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=solec0der_iact-check&metric=coverage&token=1d369feff79742df91970eb392253b10ed895e27)](https://sonarcloud.io/dashboard?id=solec0der_iact-check)
-[![Lines of Code](https://sonarcloud.io/api/project_badges/measure?project=solec0der_iact-check&metric=ncloc&token=1d369feff79742df91970eb392253b10ed895e27)](https://sonarcloud.io/dashboard?id=solec0der_iact-check)
 
 Table of content
 ----------------
--- iact-check-webapp/src/main/resources/webapp/src/app/check/steps/questions-form/questions-form.component.ts
+++ iact-check-webapp/src/main/resources/webapp/src/app/check/steps/questions-form/questions-form.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckDTO } from '../../../admin/shared/dtos/check-dto';
+import { ActivatedRoute } from '@angular/router';
+import { CheckStateService } from '../../check-state.service';
+import { QuestionCategoryDTO } from '../../../admin/shared/dtos/question-category-dto';
+import { CORE_URL } from '../../../app.config';
 
 @Component({
   selector: 'app-questions-form',
   templateUrl: './questions-form.component.html',
-  styleUrls: ['./questions-form.component.scss']
+  styleUrls: ['./questions-form.component.scss'],
 })
 export class QuestionsFormComponent implements OnInit {
+  private readonly CURRENT_STEP = 4;
 
-  constructor() { }
+  public checkDTO!: CheckDTO;
+  public questionCategoryDTO!: QuestionCategoryDTO;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly checkStateService: CheckStateService
+  ) {}
 
   ngOnInit(): void {
+    this.loadData();
   }
 
+  ngAfterViewInit(): void {
+    this.checkStateService.setStep(this.CURRENT_STEP, this.activatedRoute);
+  }
+
+  public getIconUrlByRangeQuestionId(rangeQuestionId: number): string {
+    return CORE_URL + '/api/range-questions/' + rangeQuestionId + '/icon';
+  }
+
+  private loadData(): void {
+    this.checkStateService.getActiveCheck().subscribe((checkDTO) => {
+      this.checkDTO = checkDTO;
+    });
+
+    this.checkStateService
+      .getActiveQuestionCategory()
+      .subscribe((questionCategoryDTO) => {
+        this.questionCategoryDTO = questionCategoryDTO;
+      });
+  }
 }
