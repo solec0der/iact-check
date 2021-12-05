33a58f45764c40437368cbe8efc03c8b15199bc8
Yannick Huggler <yannick@huggler.io>
2021-12-05 13:12:30 +0100

    #12 -  Minor brush ups in the questions overview

--- iact-check-webapp/src/main/resources/webapp/src/app/admin/customers/customer-detail/checks/check-detail/question-category-detail/image-question-list/image-question-list.component.ts
+++ iact-check-webapp/src/main/resources/webapp/src/app/admin/customers/customer-detail/checks/check-detail/question-category-detail/image-question-list/image-question-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { QuestionCategoryDTO } from '../../../../../../../shared/dtos/question-category-dto';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageQuestionService } from '../../../../../../shared/services/image-question.service';
+import { ConfirmDialogComponent } from '../../../../../../../shared/dialogs/confirm-dialog/confirm-dialog.component';
+import { TranslateService } from '@ngx-translate/core';
+import { SnackBarService } from '../../../../../../../shared/services/snack-bar.service';
+import { MatTab } from '@angular/material/tabs';
+import { MatTable } from '@angular/material/table';
+import { ImageQuestionDTO } from '../../../../../../../shared/dtos/image-question-dto';
 
 @Component({
   selector: 'app-image-question-list',
@@ -9,11 +17,46 @@ import { QuestionCategoryDTO } from '../../../../../../../shared/dtos/question-c
 export class ImageQuestionListComponent implements OnInit {
   @Input('questionCategory') public questionCategory!: QuestionCategoryDTO;
 
+  @ViewChild('imageQuestionsTable', { static: true })
+  private imageQuestionsTable!: MatTable<ImageQuestionDTO>;
+
   public readonly displayedColumns = ['id', 'questionText', 'actions'];
 
-  constructor() {}
+  constructor(
+    private readonly matDialog: MatDialog,
+    private readonly snackBarService: SnackBarService,
+    private readonly translateService: TranslateService,
+    private readonly imageQuestionService: ImageQuestionService
+  ) {}
 
   ngOnInit(): void {}
 
-  public showImageQuestionDeletionDialog(imageQuestionId: number): void {}
+  public showImageQuestionDeletionDialog(imageQuestionId: number): void {
+    const dialogRef = this.matDialog.open(ConfirmDialogComponent, {
+      data: {
+        title: this.translateService.instant('QUESTIONS.DELETION_DIALOG.TITLE'),
+        message: '',
+        buttonTextCancel: this.translateService.instant('QUESTIONS.DELETION_DIALOG.BUTTON_TEXT_CANCEL'),
+        buttonTextConfirm: this.translateService.instant('QUESTIONS.DELETION_DIALOG.BUTTON_TEXT_CONFIRM'),
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((hasConfirmed) => {
+      if (hasConfirmed) {
+        this.deleteImageQuestionById(imageQuestionId);
+      }
+    });
+  }
+
+  private deleteImageQuestionById(imageQuestionId: number): void {
+    this.imageQuestionService.deleteImageQuestionById(imageQuestionId).subscribe(() => {
+      this.snackBarService.open(this.translateService.instant('QUESTIONS.DELETED_MESSAGE'));
+
+      this.questionCategory.imageQuestions.splice(
+        this.questionCategory.imageQuestions.findIndex((imageQuestion) => imageQuestion.id === imageQuestionId),
+        1
+      );
+      this.imageQuestionsTable.renderRows();
+    });
+  }
 }
